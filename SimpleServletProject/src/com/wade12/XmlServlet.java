c4526cf7851e3b6b08a7f2a82fe3e4ba580eeeb3
wade12 <wadetaylor@engineer.com>
2014-01-16 13:05:45 +0000

    multiple parameters, radio buttons, array, getParameterValues

--- SimpleServletProject/src/com/wade12/XmlServlet.java
+++ SimpleServletProject/src/com/wade12/XmlServlet.java
@@ -21,9 +21,20 @@ public class XmlServlet extends HttpServlet {
 	// post parameter requires html form
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
+		PrintWriter output = response.getWriter();
 		String userName = request.getParameter("userName");
-		out.println("Dia Duit " + userName + " from doPost method");
+		String surname = request.getParameter("surname");
+		output.println("Dia Duit " + userName + " from doPost method");
+		output.println("Your surname is: " + surname);
+		String profession = request.getParameter("profession");
+		output.println("Your profession is: " + profession);
+		// String offices = request.getParameter("location");
+		String[] offices = request.getParameterValues("location");
+		output.println("Your company has offices in: " + offices.length + " cities");
+		
+		for (int i=0; i<offices.length; i++) {
+			output.println(offices[i]);
+		} // end for loop
 	} // end method doPost
 	
 } // end Class XmlServlet
