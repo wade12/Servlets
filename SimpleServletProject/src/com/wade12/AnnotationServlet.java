eb9ec0cb606f9e2037c3bf667ab5f487be3eed6b
wade12 <wadetaylor@engineer.com>
2014-01-17 12:41:23 +0000

    updated code.

--- SimpleServletProject/src/com/wade12/AnnotationServlet.java
+++ SimpleServletProject/src/com/wade12/AnnotationServlet.java
@@ -2,7 +2,6 @@ package com.wade12;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,9 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class SimpleServlet
+ * Servlet implementation class AnnotatinServlet
  */
 @WebServlet(description = "My 1st Servlet", urlPatterns = { "/AnnotationServletPath" })
+/*, 
+			initParams = { @WebInitParam( name = "defaultUser", value = "Paddy Murphy")})
+*/
 public class AnnotationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -27,3 +29,23 @@ public class AnnotationServlet extends HttpServlet {
 	} // end method doGet
 
 } // end Class AnnotationServlet
+
+/*
+response.setContentType("text/html");
+PrintWriter writer = response.getWriter();
+String userName = request.getParameter("userName");
+
+HttpSession session = request.getSession();
+ServletContext context = request.getServletContext();
+if ( (userName != "") && (userName != null) ) {
+	session.setAttribute("savedUserName", userName);
+	context.setAttribute("savedUserName", userName);
+} // end if
+
+writer.println("Request parameter has userName: " + userName);
+writer.println("Session parameter has userName: " + (String) session.getAttribute("savedUserName"));
+writer.println("Context parameter has userName: " + (String) context.getAttribute("savedUserName"));
+writer.println("Init parameter has default userName: " + this.getServletConfig().getInitParameter("defaultUser"));
+writer.println("Dia Duit in html.<h3>");
+// response.getWriter().println("Dia Duit in html - in the web browser!<h3>");
+*/
--- SimpleServletProject/src/com/wade12/XmlServlet.java
+++ SimpleServletProject/src/com/wade12/XmlServlet.java
@@ -2,17 +2,10 @@ package com.wade12;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-@WebServlet(description="Simple Sevlet", urlPatterns={ "/SimpleServletPath"}, 
-				initParams={@WebInitParam(name="defaultUser", value="Paddy Murphy")})
 
 public class XmlServlet extends HttpServlet {
 
@@ -21,41 +14,28 @@ public class XmlServlet extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter writer = response.getWriter();
 		String userName = request.getParameter("userName");
-		
-		
-		HttpSession session = request.getSession();
-		ServletContext context = request.getServletContext();
-		if ( (userName != "") && (userName != null) ) {
-			session.setAttribute("savedUserName", userName);
-			context.setAttribute("savedUserName", userName);
-		} // end if
-		// writer.println("Dia Duit " + userName + " from doGet method");
-		writer.println("Request parameter has userName: " + userName);
-		writer.println("Session parameter has userName: " + (String) session.getAttribute("savedUserName"));
-		writer.println("Context parameter has userName: " + (String) context.getAttribute("savedUserName"));
-		this.getServletConfig().getInitParameter("defaultUser");
-		
-		
 		writer.println("Dia Duit " + userName + " from doGet method");
 	} // end method doGet
 	
 	// post parameter requires html form
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
-		PrintWriter output = response.getWriter();
+		PrintWriter writer = response.getWriter();
 		String userName = request.getParameter("userName");
 		String surname = request.getParameter("surname");
-		output.println("Dia Duit " + userName + " from doPost method");
-		output.println("Your surname is: " + surname);
+		writer.println("Dia Duit " + userName + " from doPost method");
+		writer.println("Your surname is: " + surname);
 		String profession = request.getParameter("profession");
-		output.println("Your profession is: " + profession);
+		writer.println("Your profession is: " + profession);
 		// String offices = request.getParameter("location");
+		// writer.println("Your company has offices in: " + offices);
 		String[] offices = request.getParameterValues("location");
-		output.println("Your company has offices in: " + offices.length + " cities");
+		writer.println("Your company has offices in: " + offices.length + " cities");
 		
 		for (int i=0; i<offices.length; i++) {
-			output.println(offices[i]);
+			writer.println(offices[i]);
 		} // end for loop
+		
 	} // end method doPost
 	
 } // end Class XmlServlet
