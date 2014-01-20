5cf9f4ecb8f5ef1b7f4079dc8fb960b3b344d78a
wade12 <wadetaylor@engineer.com>
2014-01-20 16:58:00 +0000

    request, session and context objects.

--- SimpleServletProject/src/com/wade12/AnnotationServlet.java
+++ SimpleServletProject/src/com/wade12/AnnotationServlet.java
@@ -2,11 +2,14 @@ package com.wade12;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class AnnotatinServlet
@@ -22,30 +25,22 @@ public class AnnotationServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Dia Duit from doGet method.");
-		// PrintWriter writer = response.getWriter();
+		// System.out.println("Dia Duit from doGet method.");
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
 		// writer.println("Dia Duit in html.<h3>");
-		response.getWriter().println("Dia Duit in html - in the web browser!<h3>");
+		// response.getWriter().println("Dia Duit in html - in the web browser!<h3>");
+		String userName = request.getParameter("userName");
+		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
+			if ( (userName != "") && (userName != null) ) {
+				session.setAttribute("savedUserName", userName);
+				context.setAttribute("savedUserName", userName);
+			} // end if
+		writer.println("Request parameter has userName: " + userName);
+		writer.println("Session parameter has userName: " + (String) session.getAttribute("savedUserName"));
+		writer.println("Context parameter has userName: " + (String) context.getAttribute("savedUserName"));
+
 	} // end method doGet
 
 } // end Class AnnotationServlet
-
-/*
-response.setContentType("text/html");
-PrintWriter writer = response.getWriter();
-String userName = request.getParameter("userName");
-
-HttpSession session = request.getSession();
-ServletContext context = request.getServletContext();
-if ( (userName != "") && (userName != null) ) {
-	session.setAttribute("savedUserName", userName);
-	context.setAttribute("savedUserName", userName);
-} // end if
-
-writer.println("Request parameter has userName: " + userName);
-writer.println("Session parameter has userName: " + (String) session.getAttribute("savedUserName"));
-writer.println("Context parameter has userName: " + (String) context.getAttribute("savedUserName"));
-writer.println("Init parameter has default userName: " + this.getServletConfig().getInitParameter("defaultUser"));
-writer.println("Dia Duit in html.<h3>");
-// response.getWriter().println("Dia Duit in html - in the web browser!<h3>");
-*/
